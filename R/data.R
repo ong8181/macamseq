2fa276ffe5e08f4361da31df4b16281d1b5cf578
Masayuki Ushio <ong8181@gmail.com>
2023-06-22 09:15:40 +0800

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Demo sample data used for phyloseq object
+#'
+#' @format A data frame with 36 rows and 14 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"sample_sheet"
+
+
+#' Demo ASV table data used for phyloseq object
+#'
+#' @format A data frame with 36 rows and 358 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"asv_sheet"
+
+
+#' Demo taxa table data used for phyloseq object
+#'
+#' @format A data frame with 358 rows and 13 variables:
+#' @source \url{https://doi.org/10.1111/2041-210X.13204}
+"tax_sheet"
